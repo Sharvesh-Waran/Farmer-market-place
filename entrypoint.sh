a17cffddc264d2f03f6712266cf76f3223b8caa6
Sharveshwaran S S <146841413+Sharvesh-Waran@users.noreply.github.com>
2025-08-07 16:42:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 cd /app
 
-#echo "Running Django migrations..."
-#python3 manage.py migrate
+echo "Running Django migrations..."
+python3 manage.py migrate
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
@@ -12,5 +12,5 @@ python3 manage.py collectstatic --noinput
 echo "Starting Django..."
 python3 manage.py runserver 0.0.0.0:8000 &
 
-echo "starting Nginx..."
+echo "Starting Nginx..."
 exec nginx -g "daemon off;"
