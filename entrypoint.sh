a43797146c921c120cf4009eb73ea1114fcda69b
Sharveshwaran S S <146841413+Sharvesh-Waran@users.noreply.github.com>
2025-08-08 12:03:08 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 cd /app
 
 echo "Running Django migrations..."
-python3 manage.py migrate
+#python3 manage.py migrate
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
