8855ec73252179dd72de8a23006548d7f4824967
Sharveshwaran S S <146841413+Sharvesh-Waran@users.noreply.github.com>
2025-08-08 10:52:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,11 @@ python3 manage.py migrate
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-echo "Starting Django..."
-#python3 manage.py runserver 0.0.0.0:8000 
-python manage.py runserver 0.0.0.0:8000
-
+echo "Starting Django with OpenTelemetry..."
+opentelemetry-instrument \
+  --traces_exporter otlp \
+  --exporter_otlp_endpoint http://otel-collector-opentelemetry-collector.mointorlgpt.svc.cluster.local:4318 \
+  python manage.py runserver 0.0.0.0:8000
 
 echo "Starting Nginx..."
 exec nginx -g "daemon off;"
